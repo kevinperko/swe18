ec42033e8075d98dec0814cd7d89043ffdb2a373
thomas <thomas.7.karner@gmail.com>
2018-02-03 11:21:42 +0100

    standard test wieder aktiviert

--- swe-iot/src/main/java/org/c02/swe/iot/behaviour/CountAndShowLed.java
+++ swe-iot/src/main/java/org/c02/swe/iot/behaviour/CountAndShowLed.java
@@ -1,9 +1,11 @@
 package org.c02.swe.iot.behaviour;
 
 import java.awt.Color;
+import java.io.IOException;
 
 import org.c02.swe.iot.IButton;
 import org.c02.swe.iot.IButton.ButtonDirection;
+import org.c02.swe.iot.cloud.api.ParticleException;
 
 public class CountAndShowLed extends AbstractBehaviour {
 
@@ -13,8 +15,15 @@ public class CountAndShowLed extends AbstractBehaviour {
 
 	@Override
 	public void run() {
-		//int buttonCounter = button.getButtonClickCounter(ButtonDirection.North);
-		//button.setLed(buttonCounter, Color.GREEN);
+		int buttonCounter;
+		try {
+			buttonCounter = button.getButtonClickCounter(ButtonDirection.North);
+			button.setLed(buttonCounter, Color.GREEN);
+		} catch (IOException | ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
